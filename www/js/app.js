13d02e620fae18215c7e76ed7af83bb418535091
Bruno Silvano Costa <bruno_705@hotmail.com>
2016-05-22 18:21:08 -0300

    Added creation and instantiation of new Database.

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,8 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
+var db;
+
 angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
 
 .run(function($ionicPlatform) {
@@ -19,5 +21,8 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services',
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    // create and instantiate new Database
+    db = window.sqlitePlugin.openDatabase({ name: 'my.db', location: 'default'});
+    db.executeSql("CREATE TABLE IF NOT EXISTS contatos (phone TEXT)");
   });
 })
